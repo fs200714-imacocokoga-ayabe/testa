bdbdb0435013cec9250c6c9d71e01f1e26617dc2
imacocokoga-ayabe <ancientsuzaku@gmail.com>
2020-08-18 10:49:56 +0900

    ifの条件を20にしました #2

--- test4.java
+++ test4.java
@@ -3,13 +3,13 @@ package rennsyuuyou;
 public class test4 {
 
 	  public static void main(String[] args) {
-	    
+
 		        int n = 10;
-		    	
-if(0 <= n && n < 10){
-		    	System.out.println("0以上10未満です");
+
+if(0 <= n && n < 20){
+		    	System.out.println("0以上20未満です");
 }else{
-		    	System.out.println("0以上10未満ではありません");
+		    	System.out.println("0以上20未満ではありません");
 }
 
 	}
